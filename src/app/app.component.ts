24cbf8b51f7b956461dff1f65ce79ff72c420b71
VascoCardos <78988922+VascoCardos@users.noreply.github.com>
2022-01-31 22:58:14 +0000

    a

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  
+  selector: 'p3-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'projeto3';
+}
