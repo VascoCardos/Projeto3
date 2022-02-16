d4ca8aea0435204c5c62d05f160c19b9067142cb
VascoCardos <78988922+VascoCardos@users.noreply.github.com>
2022-02-16 20:20:53 +0000

    registar

--- src/app/pages/home/registar/registar.component.ts
+++ src/app/pages/home/registar/registar.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl,FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'p3-registar',
@@ -10,7 +13,19 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class RegistarComponent implements OnInit {
   name = new FormControl('');
   hide = true;
-  constructor(public dialogRef: MatDialogRef<RegistarComponent>) { }
+  @Input() error: string | null;
+  form: FormGroup;
+
+
+  constructor(private router: Router,private authService: AuthService, private formBuilder: FormBuilder, public dialogRef: MatDialogRef<RegistarComponent>) {
+    this.form = new FormGroup({
+			email: new FormControl('', [Validators.required, Validators.email]),
+			password: new FormControl('', Validators.required),
+      nome: new FormControl ('', Validators.required)
+		});
+		this.error = null;
+
+  }
 
   ngOnInit(): void {
   }
@@ -19,10 +34,16 @@ export class RegistarComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  profileForm = new FormGroup({
+  submit(): void {
+		if (this.form.valid) {
+			this.authService.login(
+				this.form.controls.email.value,
+				this.form.controls.password.value,
+			).subscribe(
+				(success) => this.router.navigate(['/']),
+				(err) => this.error = 'Email invalid'
+			);
+		}
+	}
 
-    email: new FormControl('', Validators.required),
-    nome: new FormControl ('', Validators.required),
-    password: new FormControl('', Validators.required)
-  });
 }
